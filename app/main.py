6d966ccb7bdbd24f8682107e74a3ab59366402fe
Mo' Sheriff <smomoh96@gmail.com>
2024-11-04 20:50:30 +0100

    Added Procfile

--- app/main.py
+++ app/main.py
@@ -1,21 +1,11 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from alembic import command
-from alembic.config import Config
 
 from . import models
 from .database import engine
 from .routers import posts, users, auth, likes
 
 # models.Base.metadata.create_all(bind=engine)
-
-def create_database():
-    # Create a Config object, pointing to the alembic.ini file
-    alembic_cfg = Config("alembic.ini")
-    print("Creating Database schemas")
-    # Run the Alembic upgrade command to apply all migrations
-    command.upgrade(alembic_cfg, "head")
-    print("Data base created successfully.")
 
 app = FastAPI()
 
@@ -32,10 +22,6 @@ app.include_router(posts.router)
 app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(likes.router)
-
-@app.on_event("startup")
-async def startup_event():
-    create_database()
 
 @app.get("/")
 def read_root():
